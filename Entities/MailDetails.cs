d7520477f2fa9e91aa82f8fdba939174f3803bd7
AvigailEtrog <Ae0527627762@gmail.com>
2024-04-01 15:28:29 +0300

    Add project files.

---
+++ Entities/MailDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mailProject._Entities
+{
+    public class MailDetails
+    {
+        [Required]
+        public int MailId { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Mail { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Phone { get; set; }
+    }
+}
